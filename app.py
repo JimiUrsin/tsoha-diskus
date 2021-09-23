b0e9bfd64683bd125813e5939f0394a25d4af7db
JimiUrsin <jimi.ursin@gmail.com>
2021-09-24 00:55:47 +0300

    Fixed typo in last commit

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def deletethread():
     thread_id = request.form["thread_id"]
     forum_id = request.form["forum_id"]
 
-    sql = "DELETE FROM threads WHERE threads.id=:thread_id AND threads.created_by=user_id;"
+    sql = "DELETE FROM threads WHERE threads.id=:thread_id AND threads.created_by=:user_id;"
     db.session.execute(sql, {"thread_id":thread_id, "user_id":session["id"]})
     db.session.commit()
     return redirect(f"/forums/{forum_id}")
